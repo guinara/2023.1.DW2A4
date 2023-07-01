ab464ea585f831a7b521c9f0fe2446e175d5f0bd
Guilherme Nara de Souza Stambonsky <gnara15@gmail.com>
2023-07-01 08:17:55 -0300

    Correções

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
+
+import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import TaskDetails from "./components/TaskDetails";
 
-const App= () => {
-  const [tasks, setTasks]=useState([
-      {
-        id: '1',
-        title:'Estudar Programação',
-        completed:false,
-      },
-      {
-        id: '2',
-        title:'Ler Livros',
-        completed:true,
-      }
-    ]);
-    const handleTaskClick = (taskId) => {
-      const newTasks = tasks.map((task) => {
-        if (task.id === taskId) return { ... task, completed: !task.completed };
-        
-        return task;
-      });
-      
-      setTasks(newTasks)
+import "./App.css";
+
+const App = () => {
+	const [tasks, setTasks]=useState([]);
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.cypress.io/todos?_limit=10"
+      );
+      setTasks(data);
     };
 
-    const handleTaskDeletion = (taskId)=>{
-      const newTasks= tasks.filter((task)=>task.id!==taskId);
-      setTasks(newTasks)
-    };
-    const handleTaskAddition = (taskTitle) => {
-      const newTasks = [
-        ... tasks,
-        {
-        title:taskTitle,
-        id: uuidv4(),
-        completed:false,
-      },
-    ];
+    fetchTasks();
+  }, []);
+  
+	const handleTaskClick = (taskId) => {
+		const newTasks = tasks.map((task) => {
+			if (task.id === taskId) return { ...task, completed: !task.completed };
 
-    setTasks(newTasks)
-    };
+			return task;
+		});
 
-  return (
-  <>
-      <div className="container">
-         <AddTask handleTaskAddition={handleTaskAddition} /> 
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
-      </div>
-    </>
-  );
+		setTasks(newTasks);
+	};
+
+	const handleTaskAddition = (taskTitle) => {
+    if (taskTitle !== "") {
+		const newTasks = [
+			...tasks,
+			{
+				title: taskTitle,
+				id: uuidv4(),
+				completed: false,
+			},
+		];
+      setTasks(newTasks);
+    }
+	};
+
+	const handleTaskDeletion = (taskId) => {
+		const newTasks = tasks.filter((task) => task.id !== taskId);
+
+		setTasks(newTasks);
+	};
+
+	return (
+		<Router>
+			<div className="container">
+				<Header />
+				<Route
+					path="/"
+					exact
+					render={() => (
+						<>
+							<AddTask handleTaskAddition={handleTaskAddition} />
+							<Tasks
+								tasks={tasks}
+								handleTaskClick={handleTaskClick}
+								handleTaskDeletion={handleTaskDeletion}
+							/>
+						</>
+					)}
+				/>
+				<Route path="/:taskTitle" exact component={TaskDetails} />
+			</div>
+		</Router>
+	);
 };
+
 export default App;
