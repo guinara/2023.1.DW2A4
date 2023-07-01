45fdc1774eb6bf0cd50ac1cb823c952946c1daa4
Guilherme <gnara15@gmail.com>
2023-07-01 00:22:04 -0300

    primerio commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Task.css';
+
+const  Task= ({task,handleTaskClick,handleTaskDeletion}) => {
+    return (
+        <div className='task-container'
+        style={task.completed ? {borderLeft: "6px solid chartreuse"}: {}}
+        >
+            <div className='task-title' onCLick={() =>handleTaskClick(task.id)}>
+            {task.title}
+            </div>
+
+            <div className='buttons-container'>
+                <button className="remove-task-button" 
+                onClick={()=>handleTaskDeletion(task.id)}>vsf</button>
+            </div>
+        </div>
+    )
+    //return <div className="task-container" >{task.title}</div>;
+}
+ 
+export default Task;
